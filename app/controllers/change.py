010fd93203003c713db1b7b44374a7f8a80aeecc
DeWuwo <767238116@qq.com>
2020-04-17 20:31:03 +0800

    修改资料

--- app/controllers/change.py
+++ app/controllers/change.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, current_app
 from flask.views import MethodView
 from sqlalchemy.exc import SQLAlchemyError
 from app.models.change import Change as MChange
+from app.models.errors import PropertyNotExist, UserNotFound, PasswordNotSatisfactory
 from app.utils import Warp, errors
 
 change = Blueprint('change', __name__)
@@ -16,6 +17,12 @@ class Change(MethodView):
         except SQLAlchemyError as e:
             current_app.logger.error(e)
             return Warp.fail_warp(501, errors['501'])
+        except (TypeError, ValueError, PropertyNotExist) as e:
+            current_app.logger.error(e)
+            return Warp.fail_warp(302, str(e))
+        except PasswordNotSatisfactory as e:
+            current_app.logger.error(e)
+            return Warp.fail_warp(203, errors['203'])
 
 
 change.add_url_rule('/change', view_func=Change.as_view('change'))
