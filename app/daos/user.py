a1f655874bf775f73c35afebcdf589de7f5d367d
DeWuwo <767238116@qq.com>
2020-04-17 10:45:26 +0800

    修改资料

--- app/daos/user.py
+++ app/daos/user.py
@@ -5,10 +5,19 @@ class IUser:
     def query_user_by_username(self, username: str) -> User:
         pass
 
+    def query_user_by_id(self, uid: int) -> User:
+        pass
+
 
 class DaoUser(IUser):
     def query_user_by_username(self, username):
         return User.query. \
             filter_by(username=username). \
+            filter_by(delete_at=None). \
+            first()
+
+    def query_user_by_id(self, uid):
+        return User.query. \
+            filter_by(id=uid). \
             filter_by(delete_at=None). \
             first()
